d62f2b61784b1d2a1c26bf879c75d2524b60dc03
Anshul Kawarey <anshulkawarey@gmail.com>
2024-12-15 20:48:35 +0530

    Finished Background design

--- hobby_app/lib/themes/dark_mode.dart
+++ hobby_app/lib/themes/dark_mode.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 ThemeData darkMode = ThemeData(
     colorScheme: ColorScheme.dark(
-  surface: Color.fromARGB(255, 90, 90, 90),
+  surface: Color.fromARGB(255, 75, 75, 75),
   primary: Color.fromARGB(255, 202, 202, 202),
   secondary: Color.fromARGB(255, 31, 31, 31),
   tertiary: Color.fromARGB(255, 103, 103, 104),
   inversePrimary: const Color.fromARGB(255, 24, 24, 24),
-  shadow: Color.fromARGB(65, 49, 212, 231),
-  primaryContainer: Color.fromARGB(69, 102, 8, 211),
+  shadow: Color.fromARGB(255, 139, 191, 204),
+  primaryContainer: Color.fromARGB(255, 80, 29, 90),
   surfaceContainer: Color.fromARGB(255, 31, 35, 38),
   // Color.fromARGB(255, 78, 78, 78),
 ));
